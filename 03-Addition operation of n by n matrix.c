df369cde49bf3a14e71a5c7cdf28e9c186a4a42c
SK <englishenglishenglish@proton.me>
2025-07-12 15:58:12 +0000

    Update 03-Addition operation of n by n matrix.c

--- 03-Addition operation of n by n matrix.c
+++ 03-Addition operation of n by n matrix.c
@@ -2,20 +2,23 @@
 int main(){
         int n;
 
+        //logic for taking input
         scanf("%d",&n);
+        
         int mtrx1[n][n];
         int mtrx2[n][n];
         int res_mtrx[n][n];
 
 
         
-
+        //logic for taking input
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 scanf("%d",&mtrx1[i][j]);
             }
         }
 
+        //logic for taking input
         for(int x=0;x<n;x++){
             for(int y=0;y<n;y++){
                 scanf("%d",&mtrx2[x][y]);
