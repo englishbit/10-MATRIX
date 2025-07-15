d2460e017dbb1b11a005986855e1d6142f238eda
SK <englishenglishenglish@proton.me>
2025-07-15 09:04:37 +0000

    Update 02-Column wise traversing of a matrix.c

--- 02-Column wise traversing of a matrix.c
+++ 02-Column wise traversing of a matrix.c
@@ -14,6 +14,7 @@
                 }
 
 
+        //logic for output
                 for(int i=0;i<n;i++){
                     for(int j=0;j<n;j++){
                         printf("%d ",matrix[j][i]);
