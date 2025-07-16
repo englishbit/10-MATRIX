22c00be883760eb77511647f07eec26606bf56ab
SK <englishenglishenglish@proton.me>
2025-07-16 08:59:29 +0000

    Update 06-Boundary traversal algorithm of n by n matrix.c

--- 06-Boundary traversal algorithm of n by n matrix.c
+++ 06-Boundary traversal algorithm of n by n matrix.c
@@ -6,6 +6,7 @@ int main(){
         scanf(" %d",&n);
         int arr[n][n];
 
+        //logic for taking the input matrix
         for (int k=0;k<n;k++){
             for(int l=0;l<n;l++){
                 scanf(" %d",&arr[k][l]);
